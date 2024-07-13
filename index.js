edbc1173345212d6457a8a36535780a67860106c
Harshit Nath <52531380+itsmehoney07@users.noreply.github.com>
2024-07-13 15:44:55 +0000

    day1

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("hello");
+var a=34;
+console.log(a)
+let b;
+b="initializing string"
+console.log(b)
+const c=true
+console.log(c)
+let z=34;
+console.log(typeof z)
+let x="";
+console.log(typeof x)
+let boolean = true
+console.log(typeof boolean)
+let obj= {
+    "name":"t"
+}
+console.log(typeof obj)
+let new_var="trtr"
+console.log(new_var)
+new_var=65
+console.log(new_var)
